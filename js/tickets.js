ead4cf35b555cf39206ba5690afc1c466c413ecd
delorenj <jaradd@gmail.com>
2010-03-29 19:48:06 -0400

    Modified ticket showcase Added mediaType selector

--- js/tickets.js
+++ js/tickets.js
@@ -5,6 +5,31 @@ $(document).ready(function() {
 	});
 });
 
+function onMediaTypeChange(val)
+{
+  //var res = $("select [name='mediaType']:selected").val();
+  var res = val;
+  var img;
+  switch(res){
+    case "1":
+      img = "images/media/external.gif";
+      break;
+    case "2":
+      img = "images/media/internal.gif";
+      break;
+    case "3":
+      img = "images/media/laptop.gif";
+      break;
+    case "4":
+      img = "images/media/flash.gif";
+      break;
+    default:
+      img = "images/media/nopicture.gif";
+  }
+
+  $("#mediaTypeResult").css("border", "none").css("background-color", "#FFFFFF").html("<img src='"+img+"'/>");
+}
+
 function initPhase2()
 {
 	$("#phase2").slideDown('slow');
--- partial/ticket_form.php
+++ partial/ticket_form.php
@@ -1,12 +1,17 @@
 <form id="ticket-form" method="post" action="validateTicket.php">
 	<div id="ticket-accordion">
-		<h3><a href="#">Select a Service</a></h3>
-		<div>
-			<ul>
-				<li><a href="#" onClick="initPhase2()"><img src="images/media/external.gif" /></a></li>
-				<li><a href="#" onClick="initPhase2()"><img src="images/media/laptop.gif" /></a></li>
-				<li><a href="#" onClick="initPhase2()"><img src="images/media/flash.gif" /></a></li>
-			</ul>			
+		<h3><a href="#">Service</a></h3>
+		<div id="serviceDiv">
+      <div class="formfield horizontal" style="width:40%;">
+        <label for="mediaType">Select a type of media:</label>
+        <select name="mediaType" onChange="onMediaTypeChange(this.value)">
+          <option value="1">External Hard Drive</option>
+          <option value="2">Internal Hard Drive</option>
+          <option value="3">Laptop Hard Drive</option>
+          <option value="4">USB/Flash Drive</option>
+        </select>
+        <div class="select-result" id="mediaTypeResult"></div>
+      </div>
 		</div>
 		<h3><a href="#">Shipping Info</a></h3>
 		<div>
