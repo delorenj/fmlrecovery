48601fe1eaf10894673bfde94a788c0af28f439c
root <root@nardis.delonet>
2010-07-14 19:31:34 -0400

    Adding a set of footer links

--- partial/footer.php
+++ partial/footer.php
@@ -1,5 +1,10 @@
 				<div id="footer">
 			  	<p>FML Recovery &copy; 2010. All Rights Reserved</p>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="#">Terms & Agreement</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
 				</div>
      </div>
    </body>
