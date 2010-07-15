c266de387a7e3f015d5f958195e7404b0485af73
root <root@sil007.(none)>
2010-07-14 20:09:59 -0400

    Formatted the footer links a bit

--- partial/footer.php
+++ partial/footer.php
@@ -1,8 +1,8 @@
 				<div id="footer">
-			  	<p>FML Recovery &copy; 2010. All Rights Reserved</p>
+			  	<p><span style="font-weight: 800; color: #93CD02;">fml</span><span style="color:#00027C;">Recovery</span>&copy; 2010. All Rights Reserved</p>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="#">Terms & Agreement</a></li>
+              <li><a href="./">Home</a></li>
+              <li><a href="#">Terms and Conditions</a></li>
               <li><a href="#">Privacy Policy</a></li>
             </ul>
 				</div>
